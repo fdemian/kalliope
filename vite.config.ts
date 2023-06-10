40dae79f7152ec7899f573816d233265cfdc0c09
fcaminiti <federicodamiancaminiti@gmail.com>
2023-06-10 06:37:04 -0300

    Fix build config.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    exclude: '/src/stories/',
+    rollupOptions: {
+      input: {
+        main: 'src/Calliope/index.ts'
+      }
+    }
   }
 })

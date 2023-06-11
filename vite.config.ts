06367603d065cd589bf47f71d89f3b9c8e5b1e54
fcaminiti <federicodamiancaminiti@gmail.com>
2023-06-11 12:24:46 -0300

    Fix production library build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  minify: true,
   build: {
-    outDir: 'build',
-    exclude: '/src/stories/',
-    rollupOptions: {
-      input: {
-        main: 'src/Calliope/index.ts'
+   exclude: '/src/stories/',
+   lib: {
+      entry: resolve(__dirname, 'src/Calliope/index.ts'),
+      name: 'kalliope',
+      fileName: 'kalliope'
+   },
+   rollupOptions: {
+    external: ['react'],
+    output: {
+      globals: {
+        react: 'React'
       }
     }
-  }
+   }
+ }
 })

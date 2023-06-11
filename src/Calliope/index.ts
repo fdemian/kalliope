d52e0aafc87a067fde9d341f69496fb6d04ad823
fcaminiti <federicodamiancaminiti@gmail.com>
2023-06-11 20:37:28 -0300

    Fix exports.

--- src/Calliope/index.ts
+++ src/Calliope/index.ts
@@ -2,9 +2,9 @@ import { getCodeLanguageOptions } from './utils/codeUtils';
 import { CODE_LANGUAGE_MAP, getLanguageFriendlyName } from '@lexical/code';
 import CalliopeEditor from './CalliopeEditor';
 
-export default CalliopeEditor;
 export {
-  getCodeLanguageOptions as getCodeLanguageOptions,
-  CODE_LANGUAGE_MAP as CODE_LANGUAGE_MAP,
-  getLanguageFriendlyName as getLanguageFriendlyName,
+  getCodeLanguageOptions,
+  CODE_LANGUAGE_MAP,
+  getLanguageFriendlyName
 };
+export default CalliopeEditor;

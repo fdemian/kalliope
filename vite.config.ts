cf306b7b0a6dd4a7e56c007713d4b9c95957146c
fcaminiti <federicodamiancaminiti@gmail.com>
2023-06-17 19:10:07 -0300

    Update storybook packages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({

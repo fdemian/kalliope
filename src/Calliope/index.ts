6f06a740442353094523cea9fe56541a782792d4
fcaminiti <federicodamiancaminiti@gmail.com>
2023-06-28 09:37:16 -0300

    Eliminate unused exports.

--- src/Calliope/index.ts
+++ src/Calliope/index.ts
@@ -2,9 +2,5 @@ import { getCodeLanguageOptions } from './utils/codeUtils';
 import { CODE_LANGUAGE_MAP, getLanguageFriendlyName } from '@lexical/code';
 import CalliopeEditor from './CalliopeEditor';
 
-export {
-  getCodeLanguageOptions,
-  CODE_LANGUAGE_MAP,
-  getLanguageFriendlyName
-};
+export { getCodeLanguageOptions }
 export default CalliopeEditor;

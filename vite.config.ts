7dc3950a63b2b4681314edd07a63603d52e42e2a
fdemian <fdemian@gmail.com>
2023-12-08 00:46:28 -0300

    Fix duplicate vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,37 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        cssInjectedByJsPlugin()
-    ],
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/Kalliope/index.ts'),
-            name: 'kalliope',
-            fileName: 'kalliope'
-        },
-        rollupOptions: {
-            external: ['react'],
-            output: {
-                globals: {
-                    react: 'React'
-                },
-                exports: "named"
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [
+            react(),
+            cssInjectedByJsPlugin()
+        ],
+        build: {
+            lib: {
+                entry: resolve(__dirname, 'src/Kalliope/index.ts'),
+                name: 'kalliope',
+                fileName: 'kalliope'
+            },
+            rollupOptions: {
+                external: ['react'],
+                output: {
+                    globals: {
+                        react: 'React'
+                    },
+                    exports: "named"
+                }
             }
-        }
-    },
+        },
+        define: {
+            // If you want to exposes all env variables, which is not recommended
+            'process.env': env,
+            module: {}
+        },
+    }
+    
 });

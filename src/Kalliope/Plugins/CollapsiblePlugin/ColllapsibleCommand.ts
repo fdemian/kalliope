a597b197a509078ce1a29f8f824d51bae6b74124
fdemian <fdemian@gmail.com>
2025-02-26 09:07:19 -0300

    Add name to collapsible command.

--- src/Kalliope/Plugins/CollapsiblePlugin/ColllapsibleCommand.ts
+++ src/Kalliope/Plugins/CollapsiblePlugin/ColllapsibleCommand.ts
@@ -1,3 +1,3 @@
 import {createCommand} from "lexical";
 
-export const INSERT_COLLAPSIBLE_COMMAND = createCommand<void>();
+export const INSERT_COLLAPSIBLE_COMMAND = createCommand<void>('INSERT_COLLAPSIBLE_COMMAND');

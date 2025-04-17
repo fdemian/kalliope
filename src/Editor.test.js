1d1813d4e961e89b076ac03bdb78c320be22d019
fdemian <fdemian@gmail.com>
2025-04-17 09:04:20 -0300

    Fix unused test.

--- src/Editor.test.js
+++ src/Editor.test.js
@@ -1,16 +1,10 @@
 import React, { useRef } from 'react';
 import TestComponentMock from './Kalliope/TestComponentMock';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 describe('<Editor />', () => {
   it('Render', () => {
-
     //const containerRef = useRef(null);
-    const containerFn = () => {
-
-    };
-
     render(
     <TestComponentMock
       containerRef={() => jest.fn()}

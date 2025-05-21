3c76a6ea4b605148b9822e11e2b675a551c7c6f9
fdemian <fdemian@gmail.com>
2025-05-20 23:26:04 -0300

    Add punctuation to emoji picker plugin.

--- src/Kalliope/Plugins/Emoji/EmojiPickerPlugin.tsx
+++ src/Kalliope/Plugins/Emoji/EmojiPickerPlugin.tsx
@@ -78,7 +78,7 @@ export default function EmojiPickerPlugin() {
 
   const checkForTriggerMatch = useBasicTypeaheadTriggerMatch(':', {
     minLength: 0,
-    
+    punctuation: '\\.,\\+\\*\\?\\$\\@\\|#{}\\(\\)\\^\\[\\]\\\\/!%\'"~=<>:;', // allow _ and -
   });
 
   const options: EmojiOption[] = useMemo(() => {
